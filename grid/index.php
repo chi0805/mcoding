725a10d4c9ed1e11225dfe85e4e76c7ce35dd49b
Chisato <c23021705@gmail.com>
2020-02-04 07:38:54 +0900

    grid 準備

---
+++ grid/index.php
@@ -0,0 +1,11 @@
+<!DEOTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>グリッドレイアウト</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+</head>
+</body>
+</body>
+</html>
